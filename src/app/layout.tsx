f6612287249f3ec03cdcbe8baa4a6e64b5f542e8
Naresh Bhatia <me@nareshbhatia.dev>
2024-07-31 14:36:22 -0400

    chore: reset to starter app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { AppProvider } from '@/providers';
 import type { Metadata } from 'next';
 import { Inter, Roboto_Mono as RobotoMono } from 'next/font/google';
 import './global.css';
@@ -17,8 +16,8 @@ const robotoMono = RobotoMono({
 });
 
 export const metadata: Metadata = {
-  title: 'Movie Magic',
-  description: 'A movie streaming application',
+  title: 'Next.js Starter',
+  description: 'Next.js Starter',
 };
 
 interface RootLayoutProps {
@@ -28,13 +27,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html
-      className={`${inter.variable} ${robotoMono.variable} dark:bg-gray-900 dark:text-white`}
+      className={`${inter.variable} ${robotoMono.variable}`}
       lang="en"
       suppressHydrationWarning
     >
-      <body className="dark:bg-gray-900 dark:text-white">
-        <AppProvider>{children}</AppProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Header from '@/components/Header';
-import MovieFilters from '@/components/MovieFilters';
-import MovieList from '@/components/MovieList';
-
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <Header />
-      <MovieFilters />
-      <MovieList />
+    <div className="page">
+      <h1 className="heading1">Next.js Starter</h1>
     </div>
   );
 }
